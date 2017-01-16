06bdef68b26863ef0b96ce83404e2f0857471fea
Ben Pritchard <ben@bennyp.org>
2017-01-16 16:59:56 -0500

    Some work on the meta build

--- pulsar.cmake
+++ pulsar.cmake
@@ -1,7 +1,25 @@
 include(ExternalProject)
 
-# Add the staged install to the prefix so that the core library can find them
-list(APPEND PULSAR_CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${STAGE_INSTALL_PREFIX})
+# RPATH handling
+# By default, add the lib directory under the install prefix
+# to the rpaths of the core
+list(APPEND PULSARMETA_INSTALL_RPATH ${CMAKE_INSTALL_PREFIX}/lib)
+list(APPEND PULSARMETA_INSTALL_RPATH ${CMAKE_INSTALL_RPATH})
+
+# Add the staged install to the cmake prefix so that the
+# core library can find them
+list(APPEND PULSARMETA_CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${STAGE_INSTALL_PREFIX})
+
+# Some arguments to pass to the core project ExternalProject_Add
+# We do it this way since some find_package may have problems if
+# a variable is set, but only to a blank string
+# For example, find_package(PythonInterp) will fail
+# if PYTHON_EXECUTABLE is a blank string
+if(PYTHON_EXECUTABLE)
+    set(PULSARMETA_EXTRA_ARGS ${PULSARMETA_EXTRA_ARGS}
+                              -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE})
+endif()
+
 
 ExternalProject_Add(pulsar_external
     GIT_REPOSITORY https://github.com/pulsar-chem/Pulsar-Core.git
@@ -14,7 +32,9 @@ ExternalProject_Add(pulsar_external
                -DCMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}
                -DCMAKE_CXX_EXTENSIONS=${CMAKE_CXX_EXTENSIONS}
                -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE}
-    CMAKE_CACHE_ARGS -DCMAKE_PREFIX_PATH:LIST=${PULSAR_CMAKE_PREFIX_PATH}
+               ${PULSARMETA_EXTRA_ARGS}
+    CMAKE_CACHE_ARGS -DCMAKE_PREFIX_PATH:LIST=${PULSARMETA_CMAKE_PREFIX_PATH}
+                     -DCMAKE_INSTALL_RPATH:LIST=${PULSARMETA_INSTALL_RPATH}
     BUILD_ALWAYS 1
     INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install DESTDIR=${STAGE_DIR}
 )
