2ba7ed296b1a25b9c15dde2f42306bb6dd375175
ryanmrichard <ryanmrichard1@gmail.com>
2017-01-19 18:15:17 -0500

    synched pybind11 w/psi4

--- pybind11.cmake
+++ pybind11.cmake
@@ -9,7 +9,7 @@ if(NOT pybind11_FOUND)
 
     ExternalProject_Add(pybind11_external
         GIT_REPOSITORY https://github.com/pybind/pybind11.git
-        GIT_TAG c79e435e00fd4fb49817a87c3c2b2647482c077b
+        GIT_TAG v2.0.0
         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
                    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
