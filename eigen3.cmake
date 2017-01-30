da99b0cb8fa4b7c5ee2d1db7eb35c0b0bf107ee5
Ryan Richard <ryanmrichard1@gmail.com>
2017-01-30 11:42:05 -0500

    switched to official eigen tarball

--- eigen3.cmake
+++ eigen3.cmake
@@ -8,8 +8,7 @@ if(NOT Eigen3_FOUND)
     message(STATUS "Eigen3 not found - we will build it for you")
 
     ExternalProject_Add(Eigen3_external
-        GIT_REPOSITORY https://github.com/RLovelett/eigen.git
-        GIT_TAG 9be3641775252951a5768ddd486977c144a519ba
+        URL http://bitbucket.org/eigen/eigen/get/3.3.2.tar.bz2
         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
                    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
